7fe275a1f2bf04b3a7c5430d44ba3fc8a07070cd
Miguel Camilo <miguelcamilo48@gmail.com>
2023-03-03 00:52:49 -0500

    CHANGED log in component UI

--- src/components/ExpenseForm/ExpenseFormList.jsx
+++ src/components/ExpenseForm/ExpenseFormList.jsx
@@ -1,8 +1,16 @@
+import { AiOutlineDelete } from "react-icons/ai";
+import { TiDeleteOutline } from "react-icons/ti";
+
 export default function ExpenseFormList({ date, title, amount }) {
 	// const dateObj = new Date();
 	// const month = dateObj.getMonth() + 1;
 	// const year = dateObj.getFullYear();
 	// const day = dateObj.getDate() + 1;
+
+	// delete request here
+	const handleDeleteExpense = (e) => {
+		alert('button clicked')
+	}
 
 	return (
 		<div className="flex selection:flex-row justify-between mt-3 text-purple-400 border-2 border-white p-3 rounded-lg">
@@ -11,6 +19,9 @@ export default function ExpenseFormList({ date, title, amount }) {
 			</p> */}
 			<p className="capitalize">{title}</p>
 			<p>{`$${amount}`}</p>
+			<button onClick={handleDeleteExpense} className="text-red-500 text-2xl">
+				<TiDeleteOutline />
+			</button>
 		</div>
 	);
 }
--- src/components/LoginCard/LoginCard.jsx
+++ src/components/LoginCard/LoginCard.jsx
@@ -19,35 +19,35 @@ const arrow = (
 
 export default function LoginCard() {
 	return (
-		<div className="flex items-center justify-center h-screen w-screen bg-[#e0ffee]">
-			<div className="relative flex flex-col m-6 space-y-10 bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0 md:m-0">
+		<div className="flex items-center justify-center h-screen w-screen bg-gray-900">
+			<div className="relative flex flex-col m-6 space-y-10 bg-[#2e3647] shadow-2xl rounded-2xl md:flex-row md:space-y-0 md:m-0">
 			<div className="p-6 md:p-20">
 				{/* content */}
-				<h2 className="mb-5 text-4xl font-bold text-center">Log In</h2>
-				<p className="max-w-sm mb-12 font-sans font-light text-gray-600 text-center ml-10">
+				<h2 className="mb-5 text-4xl font-bold text-center text-white">Log In</h2>
+				<p className="max-w-sm mb-12 font-sans font-light text-white text-center ml-10">
 					Log in to view your account
 				</p>
 				<input
 					type="text"
-					className="w-full p-6 border border-cyan-700 hover:border-cyan-500 rounded-md placeholder:font-sans placeholder:font-light"
+					className="w-full p-6 border border-purple-300 hover:border-purple-300 bg-[#2e3647] rounded-md placeholder:font-sans placeholder:font-light"
 					placeholder="Email"
 				/>
 				<input
 					type="text"
-					className="w-full p-6 mt-5 border border-cyan-700 hover:border-cyan-500 rounded-md placeholder:font-sans placeholder:font-light"
+					className="w-full p-6 mt-5 border border-purple-300 hover:border-purple-300 bg-[#2e3647] rounded-md placeholder:font-sans placeholder:font-light"
 					placeholder="Password"
 				/>
 
 				{/* middle container */}
 				<div className="flex flex-col items-center justify-between my-8 space-y-2 md:space-y-0">
-					<p className="text-cyan-800 font-light">Dont' have an account?</p>
-					<Link to="/signup" className="font-bold text-lg text-cyan-800">
+					<p className="text-purple-400 font-light">Dont' have an account?</p>
+					<Link to="/signup" className="font-bold text-lg text-purple-400">
 						Sign Up
 					</Link>
 				</div>
 
 				{/* next btn  */}
-				<button className="w-full flex justify-center items-center p-6 space-x-4 font-sans text-white rounded-md shadow-lg px-9 bg-cyan-700 shadow-slate-300 hover:bg-opacity-90 hover:shadow-2xl border transition hover:-translate-y-0.5 duration-150">
+				<button className="w-full flex justify-center items-center p-6 space-x-4 font-sans text-white rounded-md shadow-lg px-9 bg-purple-400 hover:shadow-gray-900 hover:shadow-2xl border transition hover:-translate-y-0.5 duration-150">
 					<span>{arrow}</span>
 				</button>
 
@@ -55,14 +55,14 @@ export default function LoginCard() {
 				<div className="mt-16 border-b border-gray-300"></div>
 
 				{/* Bottom Content */}
-				<p className="py-6 text-md font-light text-center text-gray-400">
+				<p className="py-6 text-md font-light text-center text-purple-400">
 					Or Login in With
 				</p>
 
 				{/* bottom buttons container */}
 				<a>
 					<div className="flex flex-col space-x-0 space-y-6 md:flex-row md:space-x-4 md:space-y-0">
-						<button className="flex items-center justify-center py-2 space-x-3 border border-gray-300 rounded-lg shadow-lg hover:shadow-2xl hover:translate-y-0.5 duration-150 w-full font-normal">
+						<button className="flex items-center justify-center py-2 space-x-3 border bg-white border-gray-300 rounded-lg shadow-lg hover:shadow-2xl hover:translate-y-0.5 duration-150 w-full font-normal">
 							<img src="/images/google.png" alt="" className="w-9" />
 							<span className="font-normal">Google</span>
 						</button>
@@ -74,7 +74,7 @@ export default function LoginCard() {
 				<img
 					src="/images/image.jpg"
 					alt="image"
-					className="w-[540px] h-full hidden md:block rounded-r-xl"
+					className="w-[450px] h-full hidden md:block rounded-r-2xl"
 				/>
 			</div>
 			</div>
