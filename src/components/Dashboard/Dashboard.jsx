5fbdb5be7d43fb402fc8e754731006b6ef39451e
Miguel Camilo <miguelcamilo48@gmail.com>
2023-03-02 19:27:14 -0500

    Expense form is complete, able to add and read information

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -4,9 +4,40 @@ import ExpenseFormList from "../ExpenseForm/ExpenseFormList";
 
 export default function Dashboard() {
 	const [expenses, setExpenses] = useState();
-	const [date, setDate] = useState();
-	const [title, setTitle] = useState();
-	// const []
+	// preventing default
+	const [title, setTitle] = useState("");
+	const [amount, setAmount] = useState("");
+	const [date, setDate] = useState("");
+
+	const handleAddExpense = (e) => {
+		e.preventDefault()
+
+		const newExpense = {
+			date: date,
+			title: title,
+			amount: amount,
+		}
+		
+		fetch(`http://localhost:3030/expenses`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(newExpense)
+		})
+		.then(res => res.json())
+		.then(setExpenses)
+		.catch(err => {
+			console.error(err)
+			alert(err.message)
+		})
+
+	}
+
+	// const handleAddIncome = () => {
+	// 	e.preventDefault()
+		
+	// }
 
 	useEffect(() => {
 		fetch(`http://localhost:3030/expenses`)
@@ -22,7 +53,7 @@ export default function Dashboard() {
 				<div className="pt-6 px-4">
 					<div className="w-full grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-4 content-end">
 						{/* expense form */}
-						<form action="">
+						<form onSubmit={handleAddExpense}>
 							<div className="bg-[#f4f5f6] shadow rounded-lg p-4 sm:p-6 xl:p-8">
 								<div className="mb-4 flex items-center justify-between">
 									<div>
@@ -34,8 +65,8 @@ export default function Dashboard() {
 										<input
 											type="date"
 											className="rounded-md border-2 border-black ml-1 text-center"
-											// value={}
-											// onChange={}
+											value={date}
+											onChange={e => setDate(e.target.value)}
 										/>
 									</div>
 								</div>
@@ -44,24 +75,24 @@ export default function Dashboard() {
 										<div className="align-middle inline-block min-w-full">
 											<div className="shadow overflow-hidden sm:rounded-lg space-x-5 flex justify-center">
 												{/* form inputs */}
-												<label htmlFor="title">Title</label>
+												<label className="mt-3" htmlFor="title">Title</label>
 												<input
 													type="text"
 													placeholder="Title"
 													className="rounded-md border-2 border-black text-center"
-													// value={}
-													// onChange={}
+													value={title}
+													onChange={e => setTitle(e.target.value)}
 												/>
 
-												<label htmlFor="amount">Amount</label>
+												<label className="mt-3" htmlFor="amount">Amount</label>
 												<input
 													type="text"
 													placeholder="Amount"
 													className="rounded-md border-2 border-black text-center"
-													// value={}
-													// onChange={}
+													value={amount}
+													onChange={e => setAmount(e.target.value)}
 												/>
-												<button className="bg-[#13e372] p-2 rounded-lg">Click</button>
+												<button type="submit" className="bg-[#13e372] p-2 mt-1 rounded-lg hover:-translate-x-1 hover:-translate-y-1 hover:drop-shadow-xl duration-300 ">Click</button>
 											</div>
 										</div>
 									</div>
@@ -98,40 +129,23 @@ export default function Dashboard() {
 							</div>
 						</div>
 					</div>
+
+					{/* second row */}
+					<div className="bg-[#f4f5f6] shadow rounded-lg p-4 sm:p-6 xl:p-8 mt-10">
+							<div className="mb-4 flex items-center justify-between">
+								<div>Charts</div>
+								<div className="flex-shrink-0"></div>
+							</div>
+							<div className="flex flex-col mt-8">
+								<div className="overflow-x-auto rounded-lg">
+									<div className="align-middle inline-block min-w-full">
+										<div className="shadow overflow-hidden sm:rounded-lg"></div>
+									</div>
+								</div>
+							</div>
+						</div>
 				</div>
 			</main>
 		</section>
 	);
-}
-
-{
-	/* expense form */
-}
-{
-	/* <form action="">
-<div
-	className="flex flex-row pb-52 px-10 mt-5 ml-5 space-y-5 md:space-x-9"
-	style={{ border: "1px solid black" }}
->
-	<input
-		type="date"
-		placeholder="Date"
-		className="rounded-lg p-1 mt-4"
-	/>
-
-	<label>Expense</label>
-	<input
-		type="text"
-		placeholder="Expense"
-		className="rounded-lg p-1"
-	/>
-
-	<label>Amount</label>
-	<input
-		type="text"
-		placeholder="Amount"
-		className="rounded-lg p-1"
-	/>
-</div>
-</form> */
 }
--- src/components/ExpenseForm/ExpenseFormList.jsx
+++ src/components/ExpenseForm/ExpenseFormList.jsx
@@ -1,9 +1,16 @@
 export default function ExpenseFormList({ date, title, amount }) {
-
 	
+	const dateObj = new Date(date)
+	const month = dateObj.getMonth() + 1
+	const year = dateObj.getFullYear()
+	const day = dateObj.getDate() + 1
+
+	// delete form
+	
+
 	return (
 		<div className="flex flex-col md:flex-row justify-between mt-3 ">
-			<p>{date}</p>
+			<p>{month}-{day}-{year}</p>
 			<p>{title}</p>
 			<p>{amount}</p>
 		</div>
