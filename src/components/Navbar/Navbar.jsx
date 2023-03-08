2b967589d6889d43c0edaaa744413003fe69caa8
Miguel Camilo <miguelcamilo48@gmail.com>
2023-03-08 08:19:15 -0500

    Before adding tailwind elements library

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
-
+import { BiMenuAltRight } from "react-icons/bi";
+import { FiSettings } from "react-icons/fi";
 
 export default function Navbar() {
 	return (
@@ -6,65 +7,31 @@ export default function Navbar() {
 			<div className="px-3 py-3 lg:px-5 lg:pl-3">
 				<div className="flex items-center justify-between">
 					<div className="flex items-center justify-start">
-						{/* <button
-							id="toggleSidebarMobile"
-							aria-expanded="true"
-							aria-controls="sidebar"
-							className="lg:hidden mr-2 text-gray-600 hover:text-gray-900 cursor-pointer p-2 hover:bg-gray-100 focus:bg-gray-100 focus:ring-2 focus:ring-gray-100 rounded"
-						>
-							<svg
-								id="toggleSidebarMobileHamburger"
-								className="w-6 h-6"
-								fill="currentColor"
-								viewBox="0 0 20 20"
-								xmlns="http://www.w3.org/2000/svg"
-							>
-								<path
-									fillRule="evenodd"
-									d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-									clipRule="evenodd"
-								/>
-							</svg>
-							<svg
-								id="toggleSidebarMobileClose"
-								className="w-6 h-6 hidden"
-								fill="currentColor"
-								viewBox="0 0 20 20"
-								xmlns="http://www.w3.org/2000/svg"
-							>
-								<path
-									fillRule="evenodd"
-									d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-									clipRule="evenodd"
-								/>
-							</svg>
-						</button> */}
-                        {/* logo */}
+						{/* logo */}
 						<a
 							href="#"
 							className="text-xl font-bold flex items-center lg:ml-2.5"
 						>
-							<span className="self-center whitespace-nowrap">Budget Buddy</span>
+							<span className="self-center whitespace-nowrap">
+								Budget Buddy
+							</span>
 						</a>
 					</div>
 
 					<div className="flex items-center">
-	
 						<div className="hidden lg:flex items-center">
-							<div className="-mb-1">
-								<a
-									className="bg-red-400 p-2 rounded-xl text-white drop-shadow-xl hover:drop-shadow-2xl"
-									href="#"
-								>
-									Log Out
-								</a>
+							<div className="-mb-2">
+								<button>
+									{/* add modal stating feature coming soon */}
+									<FiSettings className="text-2xl duration-150" />
+								</button>
 							</div>
 						</div>
 						<a
-							href="#"
-							className="md:hidden sm:inline-flex ml-5 text-white bg-blue-400 hover:bg-blue-200  font-medium rounded-lg text-sm p-2 text-center items-center mr-3"
+							className="md:hidden sm:inline-flex ml-5 text-black hover:bg-gray-100 font-medium rounded-lg text-3xl p-2 text-center items-center mr-3"
 						>
-							Make this  drop down menu
+							{/* drop down menu */}
+							<BiMenuAltRight />
 						</a>
 					</div>
 				</div>
@@ -72,135 +39,3 @@ export default function Navbar() {
 		</nav>
 	);
 }
-
-// import { Link } from "react-router-dom";
-// import { useState, useEffect } from "react";
-// import {
-// 	Navbar,
-// 	MobileNav,
-// 	Typography,
-// 	Button,
-// 	IconButton,
-// } from "@material-tailwind/react";
-
-// export default function SideBar() {
-// 	const [openNav, setOpenNav] = useState(false);
-
-// 	useEffect(() => {
-// 		window.addEventListener(
-// 			"resize",
-// 			() => window.innerWidth >= 960 && setOpenNav(false)
-// 		);
-// 	}, []);
-
-// 	const navList = (
-// 		<ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-// 			<Typography
-// 				as="li"
-// 				variant="small"
-// 				color="blue-gray"
-// 				className="p-1 font-normal"
-// 			>
-// 				<a href="#" className="flex items-center">
-// 					Pages
-// 				</a>
-// 			</Typography>
-// 			<Typography
-// 				as="li"
-// 				variant="small"
-// 				color="blue-gray"
-// 				className="p-1 font-normal"
-// 			>
-// 				<a href="#" className="flex items-center">
-// 					Account
-// 				</a>
-// 			</Typography>
-// 			<Typography
-// 				as="li"
-// 				variant="small"
-// 				color="blue-gray"
-// 				className="p-1 font-normal"
-// 			>
-// 				<a href="#" className="flex items-center">
-// 					Blocks
-// 				</a>
-// 			</Typography>
-// 			<Typography
-// 				as="li"
-// 				variant="small"
-// 				color="blue-gray"
-// 				className="p-1 font-normal"
-// 			>
-// 				<a href="#" className="flex items-center">
-// 					Docs
-// 				</a>
-// 			</Typography>
-// 		</ul>
-// 	);
-
-// 	return (
-// 		<Navbar className="mx-auto max-w-screen-xl py-2 px-4 lg:px-8 lg:py-4 drop-shadow-2xl">
-// 			<div className="container mx-auto flex items-center justify-between text-blue-gray-900">
-// 				<Typography
-// 					as="a"
-// 					href="/dashboard"
-// 					variant="small"
-// 					className="mr-4 cursor-pointer py-1.5 font-normal"
-// 				>
-// 					<button class="px-6 py-2 font-semibold tracking-wide text-white capitalize transition-colors duration-300 transform bg-[#D076FF] rounded-lg hover:bg-[#d69bf6] focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80 drop-shadow-xl">
-// 						Budget Buddy
-// 					</button>
-// 				</Typography>
-// 				<div className="hidden lg:block">{navList}</div>
-// 				<Button size="sm" className="hidden lg:inline-block">
-// 					<span>Log Out</span>
-// 				</Button>
-// 				<IconButton
-// 					variant="text"
-// 					className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-// 					ripple={false}
-// 					onClick={() => setOpenNav(!openNav)}
-// 				>
-// 					{openNav ? (
-// 						<svg
-// 							xmlns="http://www.w3.org/2000/svg"
-// 							fill="none"
-// 							className="h-6 w-6"
-// 							viewBox="0 0 24 24"
-// 							stroke="currentColor"
-// 							strokeWidth={2}
-// 						>
-// 							<path
-// 								strokeLinecap="round"
-// 								strokeLinejoin="round"
-// 								d="M6 18L18 6M6 6l12 12"
-// 							/>
-// 						</svg>
-// 					) : (
-// 						<svg
-// 							xmlns="http://www.w3.org/2000/svg"
-// 							className="h-6 w-6"
-// 							fill="none"
-// 							stroke="currentColor"
-// 							strokeWidth={2}
-// 						>
-// 							<path
-// 								strokeLinecap="round"
-// 								strokeLinejoin="round"
-// 								d="M4 6h16M4 12h16M4 18h16"
-// 							/>
-// 						</svg>
-// 					)}
-// 				</IconButton>
-// 			</div>
-// 			<MobileNav open={openNav}>
-// 				<div className="container mx-auto">
-// 					{navList}
-// 					<Button size="sm" fullWidth className="mb-2 ">
-// 						<span>Log out</span>
-// 					</Button>
-// 				</div>
-// 			</MobileNav>
-// 		</Navbar>
-// 	);
-// }
--- src/components/Navbar/SideBar.jsx
+++ src/components/Navbar/SideBar.jsx
@@ -1,29 +1,26 @@
+import { Link } from "react-router-dom";
+import { AiFillHome } from "react-icons/ai";
+import { MdSavings, MdForum } from "react-icons/md"
+import { FiLogOut } from "react-icons/fi"
+
 export default function Sidebar() {
 	return (
 		<aside
 			id="sidebar"
-			className="fixed hidden z-20 h-full top-0 left-0 pt-16 flex lg:flex flex-shrink-0 flex-col w-64 transition-width duration-75"
+			className="fixed h-full top-0 left-0 pt-16 flex lg:flex flex-shrink-0 flex-col w-64 transition-width duration-75"
 			aria-label="Sidebar"
 		>
 			<div className="relative flex-1 flex flex-col min-h-0 border-r border-gray-200 bg-white pt-0">
 				<div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
-					<div className="flex-1 px-3 bg-white divide-y space-y-1">
-						<ul className="space-y-2 pb-2">
+					<div className="flex-1 px-3 bg-white divide-y-2 space-y-1">
+						<ul className="space-y-5 pb-2">
 							<li>
 								{/* change this to router link */}
 								<a
 									href="#"
 									className="text-base text-gray-900 font-normal rounded-lg flex items-center p-2 hover:bg-gray-100 group"
 								>
-									<svg
-										className="w-6 h-6 text-gray-500 group-hover:text-gray-900 transition duration-75"
-										fill="currentColor"
-										viewBox="0 0 20 20"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z" />
-										<path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z" />
-									</svg>
+									<AiFillHome className="text-2xl" />
 									<span className="ml-3">Dashboard</span>
 								</a>
 							</li>
@@ -33,15 +30,7 @@ export default function Sidebar() {
 									href="#"
 									className="text-base text-gray-900 font-normal rounded-lg flex items-center p-2 hover:bg-gray-100 group"
 								>
-									<svg
-										className="w-6 h-6 text-gray-500 group-hover:text-gray-900 transition duration-75"
-										fill="currentColor"
-										viewBox="0 0 20 20"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z" />
-										<path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z" />
-									</svg>
+									<MdSavings className="text-2xl"/>
 									<span className="ml-3">Savings</span>
 								</a>
 							</li>
@@ -50,18 +39,7 @@ export default function Sidebar() {
 									href="#"
 									className="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group "
 								>
-									<svg
-										className="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75"
-										fill="currentColor"
-										viewBox="0 0 20 20"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<path
-											fillRule="evenodd"
-											d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-											clipRule="evenodd"
-										/>
-									</svg>
+									<MdForum className="text-2xl"/>
 									<span className="ml-3 flex-1 whitespace-nowrap">
 										Community Forum
 									</span>
@@ -71,40 +49,10 @@ export default function Sidebar() {
 						<div className="space-y-2 pt-2">
 							<a
 								href="#"
-								className="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group "
+								className="text-base text-gray-900 font-normal rounded-lg hover:bg-red-100 flex items-center p-2 group "
 							>
-								<svg
-									className="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75"
-									fill="currentColor"
-									viewBox="0 0 20 20"
-									xmlns="http://www.w3.org/2000/svg"
-								>
-									<path
-										fillRule="evenodd"
-										d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z"
-										clipRule="evenodd"
-									/>
-								</svg>
-								<span className="ml-3 flex-1 whitespace-nowrap">Sign In</span>
-							</a>
-
-							<a
-								href="#"
-								className="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group "
-							>
-								<svg
-									className="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75"
-									fill="currentColor"
-									viewBox="0 0 20 20"
-									xmlns="http://www.w3.org/2000/svg"
-								>
-									<path
-										fillRule="evenodd"
-										d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z"
-										clipRule="evenodd"
-									/>
-								</svg>
-								<span className="ml-3 flex-1 whitespace-nowrap">Log Out</span>
+								<FiLogOut className="text-2xl text-red-600"/>
+								<span className="ml-3 flex-1 text-red-600">Log Out</span>
 							</a>
 						</div>
 					</div>
