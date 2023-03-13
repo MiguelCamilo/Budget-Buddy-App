3b3048ec397964afbe8db4ef65cc351799f501e1
Miguel Camilo <miguelcamilo48@gmail.com>
2023-03-13 13:24:57 -0400

    adjusted footer

--- src/components/ChartJS/MiddleBarChart.jsx
+++ src/components/ChartJS/MiddleBarChart.jsx
@@ -2,32 +2,20 @@
 export default function MiddleBarChart() {
 
     return (
-        <div className="mt-4 w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+        <div className="mt-4 w-full md:grid-cols-2 xl:grid-cols-3 gap-4">
           {/* box 1  */}
         <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8 ">
           <div className="flex items-center">
             <div className="flex-shrink-0">
-              * Expense Bar Chart *
+              {/* * Expense Bar Chart * */}
             </div>
           </div>
         </div>
 
         {/* box 2 */}
-        <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8 ">
-          <div className="flex items-center">
-            <div className="flex-shrink-0">
-              
-            </div>
-          </div>
-        </div>
+
         {/* box 3 */}
-        <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8 ">
-          <div className="flex items-center">
-            <div className="flex-shrink-0">
-              * Income Bar Chart *
-            </div>
-          </div>
-        </div>
+
       </div>
     )
 }
