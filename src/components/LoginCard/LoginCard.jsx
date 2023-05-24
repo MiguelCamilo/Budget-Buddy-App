b12177d3a07779e1c085b4aca09bce3f9820da54
Miguel Camilo <miguelcamilo48@gmail.com>
2023-05-24 17:08:57 -0400

    FIXED bug with unresponsive right side of screen on mobile devices

--- src/components/LoginCard/LoginCard.jsx
+++ src/components/LoginCard/LoginCard.jsx
@@ -36,8 +36,8 @@ export default function LoginCard() {
 		try {
 			const promise = await signInWithEmailAndPassword(auth, email, password);
 			return promise;
-		} catch(err) {
-			console.error(err)
+		} catch (err) {
+			console.error(err);
 		}
 	};
 
@@ -45,7 +45,7 @@ export default function LoginCard() {
 	useEffect(() => {
 		const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
 			setUser(currentUser);
-			console.log('User: ', currentUser)
+			console.log("User: ", currentUser);
 		});
 		return () => {
 			unsubscribe();
@@ -63,7 +63,7 @@ export default function LoginCard() {
 	return (
 		<>
 			{/* Global Container */}
-			<div className="h-full w-full md:w-[50%] fixed z-[1] top-0 overflow-hidden pt-20 bg-white">
+			<div className="h-full w-full md:w-[50%] fixed z-[1] top-0 overflow-hidden pt-6 md:pt-20 bg-white">
 				{/* Left Side */}
 				<div className="w-full px-6 py-8 md:px-8">
 					<div className="flex justify-center mx-auto">
@@ -84,38 +84,37 @@ export default function LoginCard() {
 						our easy-to-use expense tracking and budgeting tools. Let's get
 						started!
 					</ScrollReveal.h2>
-					<ScrollReveal.div 
-						delay={0.4} 
+					<ScrollReveal.div
+						delay={0.4}
 						easing="anticipate"
-						className="flex items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg border-2 border-gray-700 text-gray-200 hover:bg-gray-100"
+						className="flex items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border-2 rounded-lg border-gray-700 hover:bg-gray-100"
 					>
-
-							<div className="px-4 py-2">
-								<svg className="w-6 h-6" viewBox="0 0 40 40">
-									<path
-										d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
-										fill="#FFC107"
-									/>
-									<path
-										d="M5.25497 12.2425L10.7308 16.2583C12.2125 12.59 15.8008 9.99999 20 9.99999C22.5491 9.99999 24.8683 10.9617 26.6341 12.5325L31.3483 7.81833C28.3716 5.04416 24.39 3.33333 20 3.33333C13.5983 3.33333 8.04663 6.94749 5.25497 12.2425Z"
-										fill="#FF3D00"
-									/>
-									<path
-										d="M20 36.6667C24.305 36.6667 28.2167 35.0192 31.1742 32.34L26.0159 27.975C24.3425 29.2425 22.2625 30 20 30C15.665 30 11.9842 27.2359 10.5975 23.3784L5.16254 27.5659C7.92087 32.9634 13.5225 36.6667 20 36.6667Z"
-										fill="#4CAF50"
-									/>
-									<path
-										d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.7592 25.1975 27.56 26.805 26.0133 27.9758C26.0142 27.975 26.015 27.975 26.0158 27.9742L31.1742 32.3392C30.8092 32.6708 36.6667 28.3333 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
-										fill="#1976D2"
-									/>
-								</svg>
-							</div>
-							<button
-								onClick={handleContinueGoogle}
-								className="w-5/6 px-4 py-3 font-bold text-center"
-							>
-								Continue with Google
-							</button>
+						<div className="px-4 py-2">
+							<svg className="w-6 h-6" viewBox="0 0 40 40">
+								<path
+									d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
+									fill="#FFC107"
+								/>
+								<path
+									d="M5.25497 12.2425L10.7308 16.2583C12.2125 12.59 15.8008 9.99999 20 9.99999C22.5491 9.99999 24.8683 10.9617 26.6341 12.5325L31.3483 7.81833C28.3716 5.04416 24.39 3.33333 20 3.33333C13.5983 3.33333 8.04663 6.94749 5.25497 12.2425Z"
+									fill="#FF3D00"
+								/>
+								<path
+									d="M20 36.6667C24.305 36.6667 28.2167 35.0192 31.1742 32.34L26.0159 27.975C24.3425 29.2425 22.2625 30 20 30C15.665 30 11.9842 27.2359 10.5975 23.3784L5.16254 27.5659C7.92087 32.9634 13.5225 36.6667 20 36.6667Z"
+									fill="#4CAF50"
+								/>
+								<path
+									d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.7592 25.1975 27.56 26.805 26.0133 27.9758C26.0142 27.975 26.015 27.975 26.0158 27.9742L31.1742 32.3392C30.8092 32.6708 36.6667 28.3333 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
+									fill="#1976D2"
+								/>
+							</svg>
+						</div>
+						<button
+							onClick={handleContinueGoogle}
+							className="w-5/6 px-4 py-3 font-bold text-center"
+						>
+							Continue with Google
+						</button>
 					</ScrollReveal.div>
 					<ScrollReveal.div delay={0.6} easing="anticipate">
 						<div className="flex items-center justify-between mt-4">
@@ -141,22 +140,22 @@ export default function LoginCard() {
 									type="email"
 								/>
 							</div>
-							<div className="mt-4">
-								<div className="flex justify-between">
-									<label
-										className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200"
-										htmlFor="loggingPassword"
-									>
-										Password
-									</label>
-								</div>
-								<input
-									value={password}
-									onChange={(e) => setPassword(e.target.value)}
-									className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-green-400 focus:ring-opacity-40 dark:focus:border-green-300 focus:outline-none focus:ring focus:ring-green-300"
-									type="password"
-								/>
+
+							<div className="flex justify-between mt-4">
+								<label
+									className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200"
+									htmlFor="loggingPassword"
+								>
+									Password
+								</label>
 							</div>
+							<input
+								value={password}
+								onChange={(e) => setPassword(e.target.value)}
+								className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-green-400 focus:ring-opacity-40 dark:focus:border-green-300 focus:outline-none focus:ring focus:ring-green-300"
+								type="password"
+							/>
+
 							<div className="mt-6">
 								<button className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gradient rounded-lg focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50 hover:opacity-90">
 									Sign In
@@ -176,7 +175,7 @@ export default function LoginCard() {
 					</ScrollReveal.div>
 				</div>
 				{/* Right Side */}
-				<div className="h-full w-[50%] fixed z-[1] top-0 overflow-hidden right-0">
+				<div className="h-full w-[50%] fixed -z-[1] top-0 overflow-hidden right-0">
 					<div
 						className="w-full h-full gradient-bg hidden md:block bg-cover"
 						style={{
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -67,7 +67,7 @@ export default function Register() {
 	return (
 		<>
 			{/* Global Container */}
-			<div className="h-full w-full md:w-[50%] fixed z-[1] top-0 overflow-hidden pt-20 bg-white">
+			<div className="h-full w-full md:w-[50%] fixed z-[1] top-0 overflow-hidden pt-4 md:pt-20 bg-white">
 				{/* Left Side */}
 				<div className="w-full px-6 py-8 md:px-8">
 					<div className="flex justify-center mx-auto">
@@ -125,22 +125,22 @@ export default function Register() {
 									type="email"
 								/>
 							</div>
-							<div className="mt-4">
-								<div className="flex justify-between">
-									<label
-										className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200"
-										htmlFor="loggingPassword"
-									>
-										Password
-									</label>
-								</div>
-								<input
-									value={password}
-									onChange={(e) => setPassword(e.target.value)}
-									className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-green-400 focus:ring-opacity-40 dark:focus:border-green-300 focus:outline-none focus:ring focus:ring-green-300"
-									type="password"
-								/>
+
+							<div className="flex justify-between mt-4">
+								<label
+									className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200"
+									htmlFor="loggingPassword"
+								>
+									Password
+								</label>
 							</div>
+							<input
+								value={password}
+								onChange={(e) => setPassword(e.target.value)}
+								className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-green-400 focus:ring-opacity-40 dark:focus:border-green-300 focus:outline-none focus:ring focus:ring-green-300"
+								type="password"
+							/>
+
 							<div className="mt-6">
 								<button className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gradient rounded-lg focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50 hover:opacity-90">
 									Sign Up
@@ -151,7 +151,7 @@ export default function Register() {
 						<div className="flex items-center justify-between mt-4">
 							<span className="w-1/5 border-b dark:border-gray-600 md:w-1/4" />
 							<Link to="/">
-								<span className="text-xs text-center uppercase text-black hover:underline">
+								<span className="text-[10px] text-center uppercase text-black hover:underline">
 									Already have an account: Login
 								</span>{" "}
 							</Link>
@@ -160,9 +160,9 @@ export default function Register() {
 					</ScrollReveal.div>
 				</div>
 				{/* Right Side */}
-				<div className="h-full w-[50%] fixed z-[1] top-0 overflow-hidden right-0">
+				<div className="h-full w-[50%] fixed -z-[1] top-0 overflow-hidden right-0">
 					<div
-						className="w-full h-full gradient-bg hidden md:block bg-cover"
+						className="w-full h-full gradient-bg bg-cover hidden md:flex"
 						style={{
 							backgroundImage:
 								'url("https://images.unsplash.com/photo-1561414926-7f3f921a2e18?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1036&q=80")',
